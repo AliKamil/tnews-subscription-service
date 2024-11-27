c43a3525f648815752e1652bede700d275f419cb
Kirill Krivonogov <hmogig@gmail.com>
2024-11-27 16:46:37 +0300

    Add connection to DB, create UserService and UserRepository

--- src/main/java/tnews/entity/User.java
+++ src/main/java/tnews/entity/User.java
@@ -1,16 +1,28 @@
 package tnews.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "user_account")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    @OneToOne(cascade = CascadeType.ALL)
     private Subscription subscription;
-
+    @CreationTimestamp
+    private LocalDateTime createdTime;
+    @UpdateTimestamp
+    private LocalDateTime updatedTime;
 }
