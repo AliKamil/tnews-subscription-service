e11b50b54a900c4c3056d75973b165bd4620ea94
Kirill Krivonogov <hmogig@gmail.com>
2024-11-27 16:46:37 +0300

    Add connection to DB, create UserService and UserRepository

---
+++ src/main/java/tnews/service/UserService.java
@@ -0,0 +1,36 @@
+package tnews.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import tnews.entity.User;
+import tnews.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(Long id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
