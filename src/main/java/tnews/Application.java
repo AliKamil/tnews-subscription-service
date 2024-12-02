c981b6eeab8edc28e262058b6506f23c5655c17c
Kirill Krivonogov <hmogig@gmail.com>
2024-12-02 22:29:40 +0300

    Fix User entity

--- src/main/java/tnews/Application.java
+++ src/main/java/tnews/Application.java
@@ -3,11 +3,17 @@ package tnews;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import tnews.bot.Bot;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(new Bot());
         SpringApplication.run(Application.class, args);
     }
 }
