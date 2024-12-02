96af93912e3d85dc89c43f9bab51a2a44f92140c
SHVERR <shevlad82@gmail.com>
2024-12-02 03:30:55 +0300

    Update Application.java
    убрал лишний импорт

--- src/main/java/tnews/Application.java
+++ src/main/java/tnews/Application.java
@@ -3,7 +3,6 @@ package tnews;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
 @EnableJpaRepositories
