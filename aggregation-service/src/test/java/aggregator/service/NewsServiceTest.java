0e1938232ad9e72f9d54ab42148259fcc1a5ea4b
SHVERR <shevlad82@gmail.com>
2025-01-26 02:30:39 +0300

    Подключена MongoDB. Доработана структура модулей. Fix

--- aggregation-service/src/test/java/aggregator/service/NewsServiceTest.java
+++ aggregation-service/src/test/java/aggregator/service/NewsServiceTest.java
@@ -1,17 +1,16 @@
 package aggregator.service;
 
-import aggregator.NewsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class NewsServiceTest {
+class NewsServiceTest {
    @Autowired
    private NewsService newsService;
 
     @Test
-    public void addNews() {
+    void addNews() {
         newsService.testMongo();
     }
 }
--- bot/src/main/java/tnews/entity/UserAction.java
+++
@@ -1,7 +0,0 @@
-package tnews.entity;
-
-public enum UserAction {
-    WAITING_FOR_KEYWORD,
-    UPDATE,
-    READY
-}
