e67199737aa0ececbbadba94bcf1b3a02f1ae7cc
Kirill Krivonogov <hmogig@gmail.com>
2025-02-01 20:41:48 +0300

    no JSON

---
+++ aggregation-service/src/main/java/aggregator/controllers/NewsController.java
@@ -0,0 +1,23 @@
+package aggregator.controllers;
+
+import aggregator.entity.News;
+import aggregator.service.NewsService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/news")
+@AllArgsConstructor
+public class NewsController {
+
+    private NewsService newsService;
+
+    @GetMapping("/fetch")
+    public List<News> fetchNews() {
+        return newsService.fetchAndSaveNews();
+    }
+}
