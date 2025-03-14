068b8ec558b8683de7be6fa27ffcaea68e95f8b2
KirillVsMonkey <hmogig@gmail.com>
2025-03-15 00:26:05 +0300

    Added test

--- aggregation-service/src/main/java/tnews/aggregator/client/entity/News.java
+++ aggregation-service/src/main/java/tnews/aggregator/client/entity/News.java
@@ -1,12 +1,16 @@
 package tnews.aggregator.client.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "news")
 public class News {
     @Id
